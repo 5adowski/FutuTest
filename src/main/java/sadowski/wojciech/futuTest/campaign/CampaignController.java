4aaf49f878b7057561ed35337eb70e6215c32b66
5adowski <wojciechsadowski2@gmail.com>
2022-09-20 21:27:55 +0200

    Added rest controller class for campaign

---
+++ src/main/java/sadowski/wojciech/futuTest/campaign/CampaignController.java
@@ -0,0 +1,38 @@
+package sadowski.wojciech.futuTest.campaign;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class CampaignController {
+    private CampaignService campaignService;
+
+    public CampaignController(CampaignService campaignService) {
+        this.campaignService = campaignService;
+    }
+
+    @GetMapping("/campaigns")
+    public List<Campaign> getAll(){
+        return campaignService.readAll();
+    }
+
+    @GetMapping("/campaign/{id}")
+    public Campaign get(@PathVariable Long id){
+        return campaignService.read(id);
+    }
+
+    @PostMapping(value = ("/campaign"),
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Campaign> post(@Valid @RequestBody Campaign campaign) {
+        campaignService.create(campaign);
+        return ResponseEntity.created(URI.create("/")).body(campaign);
+    }
+
+
+}
